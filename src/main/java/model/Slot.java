54ed400373a822a6e77f0f1ecd7943c47d24cdf5
BitterKass <ariacasellas@inspladelestany.cat>
2023-05-16 15:36:06 +0200

    Afegint el fitxer slot

---
+++ src/main/java/model/Slot.java
@@ -0,0 +1,13 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Slot {
+    private int quantitat;
+    private String codi_producte;
+}
